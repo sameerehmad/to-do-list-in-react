f790babc0d649ba5cae66c13ff74e05828e1d509
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2025-01-03 17:22:46 +0500

    Header added for to do list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import Head from "../../node_modules/next/head";
+
+export function Header({title, classes, textStyles=""}) {
+  return (
+  <div className={classes}>
+    <h1 className={textStyles}>{title}</h1>
+  </div>
+  );
+}
 
 export default function Page() {
   return (
-    <div className="relative min-h-screen min-w-screen bg-white" />
+    <div className="relative max-w-screen-lg mx-auto">
+      <Header classes="relative w-full bg-gray-500 text-center p-8" title="What to do now?" textStyles="" />
+    </div>
   );
 }
