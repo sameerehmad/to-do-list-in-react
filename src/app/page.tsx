165f822abb642df7755359c39cecaa7093c0bc12
Sameer Ahmad <124355466+sameerehmad@users.noreply.github.com>
2025-01-06 16:54:26 +0500

    2 add header in page for to do list (#7)
    * Header added for to do list
    
    * Header component moved to separate file
    
    * Header color scheme finalized
    
    ---------
    
    Co-authored-by: Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Header from "./components/Header/header.tsx";
 
 export default function Page() {
   return (
-    <div className="relative min-h-screen min-w-screen bg-white" />
+    <div className="relative max-w-screen-lg mx-auto">
+      <Header classes="relative w-full bg-blue-700 text-white text-center p-8 rounded-b-xl" title="What to do now?" textStyles="font-semibold text-3xl" />
+    </div>
   );
 }
