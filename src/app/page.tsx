d5551e62a4280853ceb7f3164cb16ddc1e9ba26f
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2025-01-06 16:53:15 +0500

    Header color scheme finalized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/header.tsx";
 export default function Page() {
   return (
     <div className="relative max-w-screen-lg mx-auto">
-      <Header classes="relative w-full bg-gray-500 text-center p-8" title="What to do now?" textStyles="font-semibold text-3xl" />
+      <Header classes="relative w-full bg-blue-700 text-white text-center p-8 rounded-b-xl" title="What to do now?" textStyles="font-semibold text-3xl" />
     </div>
   );
 }
