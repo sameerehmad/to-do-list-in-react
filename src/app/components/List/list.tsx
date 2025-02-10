6cad660b0be0c6db5c1da72ce133e81965242239
Sameer Ahmad <124355466+sameerehmad@users.noreply.github.com>
2025-02-10 18:44:51 +0500

    3 Sample items added (#10)
    * Sample items added
    
    * Key added to each ListItem
    
    * Keeping items non-const
    
    * Using react dynamic list
    
    * Dynamic List moved to child component
    
    * Initial list made constant
    
    * setItems removed
    
    ---------
    
    Co-authored-by: Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>

---
+++ src/app/components/List/list.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React, {useState} from 'react'
+import ListItem from '../ListItem/listItem'
+type TodoItem = {
+    id: number;
+    todo: string;
+    color: string;
+    created_at: string;
+};
+
+// const itemColors:string[] = ["blue", "green", "yellow", "red", "violet", "pink"];
+
+export default function List ({initItems} : {initItems : TodoItem[]}){
+    const [items] = useState(initItems);
+    return (
+    <ul className="relative w-full py-8 my-2 bg-gray-200 rounded-lg">
+        {items.map((item) => (
+            <ListItem key={item.id} item={item} />
+        ))}
+    </ul>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import Header from "./components/Header/header";
+import List from "./components/List/list";
+
+type TodoItem = {
+  id: number;
+  todo: string;
+  color: string;
+  created_at: string;
+};
+
+const items:TodoItem[] = [{id: 1, todo: "First item", color:"green", created_at:Date()},
+                        {id: 2, todo: "Second item", color:"violet", created_at:Date()},
+                        {id: 3, todo: "Third item", color:"red", created_at:Date()}];
 
 export default function Page() {
   return (
-    <div className="relative max-w-screen-lg mx-auto">
+    <div className="relative max-w-screen-md mx-auto">
       <Header classes="relative w-full bg-blue-700 text-white text-center p-8 rounded-b-xl" title="What to do now?" textStyles="font-semibold text-3xl" />
+      <List initItems={items} />
     </div>
   );
 }
