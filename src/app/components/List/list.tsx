3bc566fed6c6151c2e2cf1cd07d211ee6c77c302
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2025-02-10 18:36:45 +0500

    Dynamic List moved to child component

--- src/app/components/List/list.tsx
+++ src/app/components/List/list.tsx
@@ -1,3 +1,5 @@
+'use client';
+import React, {useState} from 'react'
 import ListItem from '../ListItem/listItem'
 type TodoItem = {
     id: number;
@@ -8,7 +10,8 @@ type TodoItem = {
 
 // const itemColors:string[] = ["blue", "green", "yellow", "red", "violet", "pink"];
 
-export default function List ({items} : {items : TodoItem[]}){
+export default function List ({initItems} : {initItems : TodoItem[]}){
+    const [items, setItems] = useState(initItems);
     return (
     <ul className="relative w-full py-8 my-2 bg-gray-200 rounded-lg">
         {items.map((item) => (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import React, {useState}  from 'react';
 import Header from "./components/Header/header";
 import List from "./components/List/list";
 
@@ -9,15 +8,15 @@ type TodoItem = {
   created_at: string;
 };
 
-const [items, setItems] = useState([{id: 1, todo: "First item", color:"green", created_at:Date()},
+let items:TodoItem[] = [{id: 1, todo: "First item", color:"green", created_at:Date()},
                         {id: 2, todo: "Second item", color:"violet", created_at:Date()},
-                        {id: 3, todo: "Third item", color:"red", created_at:Date()}]);
+                        {id: 3, todo: "Third item", color:"red", created_at:Date()}];
 
 export default function Page() {
   return (
     <div className="relative max-w-screen-md mx-auto">
       <Header classes="relative w-full bg-blue-700 text-white text-center p-8 rounded-b-xl" title="What to do now?" textStyles="font-semibold text-3xl" />
-      <List items={items} />
+      <List initItems={items} />
     </div>
   );
 }
