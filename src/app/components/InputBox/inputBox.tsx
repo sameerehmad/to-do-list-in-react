52fe536636a6e243069886c5d4e7bf375e664952
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2025-02-12 19:11:38 +0500

    Add button functionality added

--- src/app/components/InputBox/inputBox.tsx
+++ src/app/components/InputBox/inputBox.tsx
@@ -1,4 +1,6 @@
-export default function inputBox({type} : {type : string}) {
+import { ChangeEventHandler } from "react";
+
+export default function inputBox({type, value, onChange} : {type : string, value:string, onChange : ChangeEventHandler<HTMLInputElement>}) {
     return (
         <div className="my-2 mx-auto text-xl w-full flex justify-center">
             <input
@@ -10,6 +12,8 @@ export default function inputBox({type} : {type : string}) {
                        transform
                        focus:w-9/12"
             type={type}
+            value={value}
+            onChange={onChange}
             required
             />
         </div>
--- src/app/components/List/list.tsx
+++ src/app/components/List/list.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { randomInt } from 'crypto';
 import React, {useState} from 'react'
 import InputBox from '../InputBox/inputBox'
 import ListItem from '../ListItem/listItem'
@@ -9,16 +10,40 @@ type TodoItem = {
     created_at: string;
 };
 
-// const itemColors:string[] = ["blue", "green", "yellow", "red", "violet", "pink"];
+const itemColors:string[] = ["blue", "green", "yellow", "red", "violet", "pink"];
+const getRandomInRange = (min : number, max : number) => Math.floor(Math.random() * (max - min)) + min;
 
 export default function List ({initItems} : {initItems : TodoItem[]}){
-    const [items] = useState(initItems);
+    const [items, setItems] = useState(initItems);
+    const [input, setInput] = useState("");
+
+    function addItem() {
+        if (input.trim() === "") {return alert("Please type something important");}
+
+        let item : TodoItem = {
+            id: items.length + 1,
+            todo: input,
+            color: itemColors[getRandomInRange(0,6)],
+            created_at: Date()
+        };
+
+        setItems([item ,...items]);
+        setInput("");
+    }
+
     return (
         <div className='flex flex-col align-items-center'>
-            <InputBox type="text" />
+            <InputBox 
+                type="text"
+                value={input} 
+                onChange={(e) => setInput(e.target.value)}
+            />
             <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-full w-1/4 mx-auto my-2"
-            >Add</button>
+                className="bg-blue-500 text-white px-4 py-2 rounded-full w-1/4 mx-auto my-2"
+                onClick={addItem}
+            >
+                Add
+            </button>
             <ul className="relative w-full py-8 my-2 bg-gray-200 rounded-lg">
                 {items.map((item) => (
                     <ListItem key={item.id} item={item} />
