e1bb9ab9e92ef27788a88d33208d3a044845bca5
Muhammad Naveed Shahzad <naveedkpr+1@gmail.com>
2025-02-17 16:54:30 +0500

    hovering effect bettered

--- src/app/components/ListItem/listItem.tsx
+++ src/app/components/ListItem/listItem.tsx
@@ -8,7 +8,7 @@ export default function ListItem({item, deleteHandler} : {item : TodoItem, delet
     return (
         <li className="mx-auto my-2 px-4 py-3 w-11/12 inline-flex gap-10 justify-center items-center rounded-xl text-lg border-black border overflow-hidden group" style={{backgroundColor : item.color}}>
             <div data-svg-wrapper className="relative">
-                <svg className="stroke-2 stroke-red-700" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="w-6 h-6 stroke-2 stroke-red-700 hover:w-7 hover:h-7 transition-all" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path className="fill-transparent hover:fill-red-400" d="M12 21.35L10.55 20.03C5.4 15.36 2 12.28 2 8.5C2 5.42 4.42 3 7.5 3C9.24 3 10.91 3.81 12 5.09C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.42 22 8.5C22 12.28 18.6 15.36 13.45 20.04L12 21.35Z" fill="none"/>
                 </svg>
             </div>
